781f41fd96f52dd95e531bb5f8b97f82e9f4e2d0
mafalda-coelho <mafa.coel1@gmail.com>
2024-01-31 15:42:19 +0000

    added ai api

--- src/script.js
+++ src/script.js
@@ -1,14 +1,23 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
-  let recipeElement = document.querySelector("#recipe");
-
+function displayRecipe(response) {
   new Typewriter("#recipe", {
-    strings: " recipe here ",
+    strings: response.data.answer,
     autoStart: true,
     delay: 10,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "bfebe20e536f474847ab0co02fa8tf40";
+  let context =
+    "You are a culinary expert and love creating simple yet delicious recipes. Your mission is to generate a recipe in basic HTML. Make sure to follow the user instructions. Sign the recipe with SheCodes AI";
+  let prompt = `User instructions are: Generate a delicious and simple recipe with ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
